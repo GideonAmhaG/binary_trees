6627459f762c7fcd056a97152a42a36c2ec25b60
GideonAmhaG <gideonamha@gmail.com>
2023-03-29 15:32:42 +0300

    used func in 0 task

--- test.c
+++ test.c
@@ -13,7 +13,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	new = binary_tree_node(parent, value);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 	if (parent->left)
 	{
 		new->left = parent->left;
